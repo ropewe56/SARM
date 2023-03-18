69c78c8fe29908a785e8283428e46c5ace654b0c
wester <rolf.wester@mailbox.org>
2023-03-18 18:04:58 +0100

    add temperature increase estimation

--- docs/latex/calc.jl
+++ docs/latex/calc.jl
@@ -2,24 +2,32 @@
 SC = 1367.0
 
 function DT(a, ΔF, T)
+    # from sun = out travelling
     F0 = SC/4*(1-a)
+    local Te
     if T  <= 0.0
-        T  = (F0/σ)^(1/4)
+        # effective radiation temperature
+        Te  = (F0/σ)^(1/4)
+    else
+        Te = T
     end
-    F0  = σ*(T)^4
-    ΔT = ((F0+ΔF)/σ)^(1/4) - T
+    # upward from earth surface
+    F0  = σ*(Te)^4
+    # necessary ΔT to compensate ΔF
+    ΔT = ((F0+ΔF)/σ)^(1/4) - Te
 
-    ΔT2 = ((F0 + ΔF)/(σ*T^4) - 1) * T/4.0
+    ΔT2 = ((F0 + ΔF)/(σ*Te^4) - 1) * Te/4.0
 
-    T,ΔT,ΔT2
+    SC/4*(1-a),F0,T,ΔT,ΔT2
 end
 
-(F0+DF)/ (σ * T^4) T/4 = (1 + 4 D/T)
+# (F0+DF)/ (σ * T^4) T/4 = (1 + 4 D/T)
+# T^4 (1 + 4 D/T)
 
-T^4 (1 + 4 D/T)
-
-a  = 0.0
-T  = 280.0
-ΔF = 6.0
-T  = 280.0
+F0 = SC/4*(1-a)
+Fs  = σ*(289)^4
+Fs-F0
+a  = 0.3
+ΔF = 5.0
+T  = -289.0 
 DT(a, ΔF, T)
